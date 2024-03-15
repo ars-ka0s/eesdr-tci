4592c73c1802c9f33cabd159cba62402a39e6a5d
ars-ka0s <26339355+ars-ka0s@users.noreply.github.com>
2024-03-15 14:41:25 -0500

    Refactor: remove parameter caching

--- eesdr_tci/Listener.py
+++ eesdr_tci/Listener.py
@@ -5,15 +5,14 @@ import websockets
 from . import tci
 
 class Listener:
-    """The Listener class interacts with the TCI server by listening for parameter updates and
-    maintaining a cache of the current state.  A sender task passes formatted command strings
-    to the server.  Parameter and data stream callbacks can be registered to be notified of changes
-    of interest as many commands may be ignorable in certain use cases.
+    """The Listener class interacts with the TCI server by listening for parameter updates.
+    A sender task also passes formatted command strings & data packets to the server.
+    Parameter and data stream callbacks can be registered to be notified of changes of interest,
+    as many commands may be ignorable in certain use cases.
     """
 
     def __init__(self, uri):
         self.uri = uri
-        self._tci_params = {"system":{}, "receivers":{}}
         self._tci_param_listeners = {}
         self._tci_data_listeners = {}
         self._tci_send = None
@@ -96,41 +95,13 @@ class Listener:
             res += self._tci_data_listeners["*"]
         return res
 
-    def get_cached_param_value(self, cmd_info, rx = None, sub_rx = None):
-        """Retrieves the most recently received value for a specific parameter."""
-        if cmd_info.has_rx and (rx is None or type(rx) is not int or rx < 0):
-            raise ValueError(f"Command {command} requires specifying applicable receiver number (positive integer)")
-        if cmd_info.has_sub_rx and (sub_rx is None or type(sub_rx) is not int or sub_rx < 0):
-            raise ValueError(f"Command {command} requires specifying applicable sub-receiver/channel number (positive integer)")
-
-        if not cmd_info.has_rx:
-            if cmd_info.name in self._tci_params["system"]:
-                return self._tci_params["system"][cmd_info.name]
-
-        if not cmd_info.has_sub_rx:
-            if rx in self._tci_params["receivers"]:
-                if cmd_info.name in self._tci_params["receivers"][rx]:
-                    return self._tci_params["receivers"][rx][cmd_info.name]
-
-        if rx in self._tci_params["receivers"]:
-            if sub_rx in self._tci_params["receivers"][rx]["channels"]:
-                if cmd_info.name in self._tci_params["receivers"][rx]["channels"][sub_rx]:
-                    return self._tci_params["receivers"][rx]["channels"][sub_rx][cmd_info.name]
-
-        return None
-
-    def _callback_complete(self, task):
-        """Raises exceptions that occured within a notification callback."""
-        self._cb_tasks.discard(task)
-        task.result()
-
     def _schedule_callback(self, callback, *callback_args):
         """Schedules a notification callback, ensuring that the status is checked when complete."""
         task = asyncio.create_task(callback(*callback_args))
         task.add_done_callback(lambda task: task.result())
 
     async def _listen_main(self, ws):
-        """Coroutine that maintains parameter cache and schedules data/parameter callbacks."""
+        """Coroutine that receives from the server and schedules data/parameter callbacks."""
         while True:
             status = await ws.recv()
             if isinstance(status, bytes):
@@ -141,43 +112,27 @@ class Listener:
 
             parts = status.strip(";").split(":", 1)
             cmd_name = parts[0].upper()
-            assert(cmd_name in tci.COMMANDS)
+            assert cmd_name in tci.COMMANDS
 
             cmd_info = tci.COMMANDS[cmd_name]
             expected_params = cmd_info.total_params()
             if expected_params != 0:
-                assert(len(parts) == 2)
+                assert len(parts) == 2
 
                 cmd_params = [Listener._convert_type(v) for v in parts[1].split(",")]
-                assert(expected_params == -1 or len(cmd_params) == expected_params)
+                assert expected_params == -1 or len(cmd_params) == expected_params
 
-                if not cmd_info.has_rx:
-                    if len(cmd_params) == 1:
-                        cmd_params = cmd_params[0]
-                    self._tci_params["system"][cmd_info.name] = cmd_params
-                    for callback in self._get_param_listeners(cmd_info.name):
-                        self._schedule_callback(callback, cmd_info.name, None, None, cmd_params)
-                    continue
-
-                param_rx = cmd_params.pop(0)
-                dest = self._tci_params["receivers"]
-                if param_rx not in dest:
-                    dest[param_rx] = {}
-                dest = dest[param_rx]
+                param_rx = None
+                if cmd_info.has_rx:
+                    param_rx = cmd_params.pop(0)
 
                 param_sub_rx = None
                 if cmd_info.has_sub_rx:
                     param_sub_rx = cmd_params.pop(0)
-                    if "channels" not in dest:
-                        dest["channels"] = {}
-                    if param_sub_rx not in dest["channels"]:
-                        dest["channels"][param_sub_rx] = {}
-                    dest = dest["channels"][param_sub_rx]
 
                 if len(cmd_params) != 0:
                     if len(cmd_params) == 1:
                         cmd_params = cmd_params[0]
-                    dest[cmd_info.name] = cmd_params
                 else:
                     cmd_params = None
 
