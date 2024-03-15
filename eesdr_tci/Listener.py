e634284f7bc68070a19c5f87ac99964999ff2ade
ars-ka0s <26339355+ars-ka0s@users.noreply.github.com>
2024-03-15 14:05:27 -0500

    Refactor: docstrings

--- eesdr_tci/Listener.py
+++ eesdr_tci/Listener.py
@@ -1,8 +1,16 @@
+"""The listener module contains the main Listener class which interacts with the TCI server."""
+
 import asyncio
 import websockets
 from . import tci
 
 class Listener:
+    """The Listener class interacts with the TCI server by listening for parameter updates and
+    maintaining a cache of the current state.  A sender task passes formatted command strings
+    to the server.  Parameter and data stream callbacks can be registered to be notified of changes
+    of interest as many commands may be ignorable in certain use cases.
+    """
+
     def __init__(self, uri):
         self.uri = uri
         self._tci_params = {"system":{}, "receivers":{}}
@@ -17,6 +25,7 @@ class Listener:
 
     @staticmethod
     def _convert_type(val):
+        """Converts a string to int, float, or bool if possible, but returning the string if not."""
         try:
             return int(val)
         except:
@@ -35,6 +44,11 @@ class Listener:
         return val
 
     def add_param_listener(self, param, callback):
+        """Registers a callback to be notified of a particular parameter change.
+
+        The callback signature is (param_name, rx, sub_rx, params).
+        The special param "*" can be used to register a listener for all parameters.
+        """
         if param not in self._tci_param_listeners:
             self._tci_param_listeners[param] = []
         l = self._tci_param_listeners[param]
@@ -42,11 +56,17 @@ class Listener:
             l += [callback]
 
     def remove_param_listener(self, param, callback):
+        """Removes a parameter callback from the notification list"""
         l = self._tci_param_listeners[param]
         if callback in l:
             l.remove(callback)
 
     def add_data_listener(self, data_type, callback):
+        """Registers a callback to be notified when a particular type of data packet is received.
+
+        The callback signature is (packet).
+        The special data_type "*" can be used to register a listener for all data types.
+        """
         if data_type not in self._tci_data_listeners:
             self._tci_data_listeners[data_type] = []
         l = self._tci_data_listeners[data_type]
@@ -54,11 +74,13 @@ class Listener:
             l += [callback]
 
     def remove_data_listener(self, data_type, callback):
+        """Removes a data callback from the notification list."""
         l = self._tci_data_listeners[data_type]
         if callback in l:
             l.remove(callback)
 
     def _get_param_listeners(self, item):
+        """Retrieves the list of all parameter callbacks to notify for a particular parameter."""
         res = []
         if item in self._tci_param_listeners:
             res += self._tci_param_listeners[item]
@@ -67,6 +89,7 @@ class Listener:
         return res
 
     def _get_data_listeners(self, item):
+        """Retrieves the list of all data callbacks to notify for a particular data type."""
         res = []
         if item in self._tci_data_listeners:
             res += self._tci_data_listeners[item]
@@ -75,6 +98,7 @@ class Listener:
         return res
 
     def get_cached_param_value(self, cmd_info, rx = None, sub_rx = None):
+        """Retrieves the most recently received value for a specific parameter."""
         if cmd_info.has_rx and (rx is None or type(rx) is not int or rx < 0):
             raise ValueError(f"Command {command} requires specifying applicable receiver number (positive integer)")
         if cmd_info.has_sub_rx and (sub_rx is None or type(sub_rx) is not int or sub_rx < 0):
@@ -97,15 +121,18 @@ class Listener:
         return None
 
     def _callback_complete(self, task):
+        """Raises exceptions that occured within a notification callback."""
         self._cb_tasks.discard(task)
         task.result()
 
     def _schedule_callback(self, callback, *callback_args):
+        """Schedules a notification callback, ensuring that the status is checked when complete."""
         task = asyncio.create_task(callback(*callback_args))
         self._cb_tasks.add(task)
         task.add_done_callback(self._callback_complete)
 
     async def _listen_main(self, ws):
+        """Coroutine that maintains parameter cache and schedules data/parameter callbacks."""
         while True:
             status = await ws.recv()
             if type(status) is bytes:
@@ -165,11 +192,13 @@ class Listener:
                     self._schedule_callback(callback, cmd_info.name, None, None, None)
 
     async def _sender_main(self, ws):
+        """Coroutine that sends commands and data packets to the server."""
         while True:
             msg = await self._tci_send.get()
             await ws.send(msg)
 
     async def _launch_tasks(self):
+        """Coroutine that initiates connection and creates listener/sender tasks."""
         async with websockets.connect(self.uri) as ws:
             self._listen_task = asyncio.create_task(self._listen_main(ws))
             self._sender_task = asyncio.create_task(self._sender_main(ws))
@@ -179,9 +208,11 @@ class Listener:
                 task.cancel()
 
     async def send(self, data):
+        """Coroutine to enqueue data for sending, ensuring it reaches the queue."""
         await self._tci_send.put(data)
 
     async def start(self):
+        """Coroutine called to start the connection and listener/sender tasks."""
         if self._listen_task is None:
             self._tci_send = asyncio.Queue()
             self._connected_event = asyncio.Event()
@@ -190,7 +221,9 @@ class Listener:
             await self._connected_event.wait()
 
     async def ready(self):
+        """Coroutine to verify initial synchronization is complete before continuing."""
         await self._ready_event.wait()
 
     async def wait(self):
+        """Coroutine used to wait for connection completion."""
         await asyncio.gather(self._listen_task)
