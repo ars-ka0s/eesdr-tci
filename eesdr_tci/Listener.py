40bd299975c9814a74a845c35ed1930887768703
ars-ka0s <26339355+ars-ka0s@users.noreply.github.com>
2024-03-15 17:01:23 -0500

    Refactor: improve exceptions & errors during connection

--- eesdr_tci/Listener.py
+++ eesdr_tci/Listener.py
@@ -1,7 +1,9 @@
 """The listener module contains the main Listener class which interacts with the TCI server."""
 
 import asyncio
+from asyncio.exceptions import CancelledError
 import websockets
+
 from . import tci
 
 class Listener:
@@ -16,8 +18,7 @@ class Listener:
         self._tci_param_listeners = {}
         self._tci_data_listeners = {}
         self._tci_send = None
-        self._listen_task = None
-        self._sender_task = None
+        self._launch_task = None
         self._connected_event = None
         self._ready_event = None
 
@@ -104,6 +105,7 @@ class Listener:
         """Coroutine that receives from the server and schedules data/parameter callbacks."""
         while True:
             status = await ws.recv()
+
             if isinstance(status, bytes):
                 packet = tci.TciDataPacket.from_buf(status)
                 for callback in self._get_data_listeners(packet.data_type):
@@ -112,37 +114,44 @@ class Listener:
 
             parts = status.strip(";").split(":", 1)
             cmd_name = parts[0].upper()
-            assert cmd_name in tci.COMMANDS
+            if cmd_name not in tci.COMMANDS:
+                raise ValueError(f'Command {cmd_name} unrecognized')
 
             cmd_info = tci.COMMANDS[cmd_name]
             expected_params = cmd_info.total_params()
-            if expected_params != 0:
-                assert len(parts) == 2
 
-                cmd_params = [Listener._convert_type(v) for v in parts[1].split(",")]
-                assert expected_params == -1 or len(cmd_params) == expected_params
-
-                param_rx = None
-                if cmd_info.has_rx:
-                    param_rx = cmd_params.pop(0)
-
-                param_sub_rx = None
-                if cmd_info.has_sub_rx:
-                    param_sub_rx = cmd_params.pop(0)
-
-                if len(cmd_params) != 0:
-                    if len(cmd_params) == 1:
-                        cmd_params = cmd_params[0]
-                else:
-                    cmd_params = None
-
-                for callback in self._get_param_listeners(cmd_info.name):
-                    self._schedule_callback(callback, cmd_info.name, param_rx, param_sub_rx, cmd_params)
-            else:
+            if expected_params == 0:
                 if cmd_info.name == "READY":
                     self._ready_event.set()
                 for callback in self._get_param_listeners(cmd_info.name):
                     self._schedule_callback(callback, cmd_info.name, None, None, None)
+                continue
+
+            if len(parts) != 2:
+                raise ValueError(f'Command {cmd_name} should have parameters, but none received.')
+
+            cmd_params = [Listener._convert_type(v) for v in parts[1].split(",")]
+            param_cnt = len(cmd_params)
+            if expected_params not in (-1, param_cnt):
+                raise ValueError(f'Command {cmd_name} should have {expected_params} params, received {param_cnt}')
+
+            param_rx = None
+            if cmd_info.has_rx:
+                param_rx = cmd_params.pop(0)
+                param_cnt -= 1
+
+            param_sub_rx = None
+            if cmd_info.has_sub_rx:
+                param_sub_rx = cmd_params.pop(0)
+                param_cnt -= 1
+
+            if param_cnt == 1:
+                cmd_params = cmd_params[0]
+            elif param_cnt == 0:
+                cmd_params = None
+
+            for callback in self._get_param_listeners(cmd_info.name):
+                self._schedule_callback(callback, cmd_info.name, param_rx, param_sub_rx, cmd_params)
 
     async def _sender_main(self, ws):
         """Coroutine that sends commands and data packets to the server."""
@@ -152,31 +161,68 @@ class Listener:
 
     async def _launch_tasks(self):
         """Coroutine that initiates connection and creates listener/sender tasks."""
-        async with websockets.connect(self.uri) as ws:
-            self._listen_task = asyncio.create_task(self._listen_main(ws))
-            self._sender_task = asyncio.create_task(self._sender_main(ws))
-            self._connected_event.set()
-            done, pending = await asyncio.wait([self._listen_task, self._sender_task], return_when=asyncio.FIRST_COMPLETED)
-            for task in pending:
-                task.cancel()
+        try:
+            async with websockets.client.connect(self.uri) as ws:
+                listen_task = asyncio.create_task(self._listen_main(ws))
+                sender_task = asyncio.create_task(self._sender_main(ws))
+                self._connected_event.set()
+                try:
+                    await listen_task
+                    await sender_task
+                except CancelledError:
+                    listen_task.cancel()
+                    sender_task.cancel()
+        except CancelledError:
+            pass
 
     async def send(self, data):
         """Coroutine to enqueue data for sending, ensuring it reaches the queue."""
         await self._tci_send.put(data)
 
-    async def start(self):
-        """Coroutine called to start the connection and listener/sender tasks."""
-        if self._listen_task is None:
-            self._tci_send = asyncio.Queue()
-            self._connected_event = asyncio.Event()
-            self._ready_event = asyncio.Event()
-            asyncio.create_task(self._launch_tasks())
-            await self._connected_event.wait()
+    def send_nowait(self, data):
+        """Enqueue data for sending without ensuring it reaches the queue."""
+        self._tci_send.put_nowait(data)
 
-    async def ready(self):
+    async def start(self, timeout=3.0):
+        """Coroutine called to start the connection and listener/sender tasks."""
+        if self._launch_task is not None and not self._launch_task.done():
+            return
+        self._tci_send = asyncio.Queue()
+        self._connected_event = asyncio.Event()
+        self._ready_event = asyncio.Event()
+        self._launch_task = asyncio.create_task(self._launch_tasks())
+        async def _await_conn():
+            await self._connected_event.wait()
+        done, _ = await asyncio.wait([asyncio.create_task(_await_conn())], timeout=timeout)
+        if len(done) == 0:
+            self._launch_task.cancel()
+            try:
+                await self._launch_task
+            except CancelledError:
+                pass
+            except Exception as exc:
+                raise TimeoutError(f'Connected event not received after {timeout} sec.') from exc
+            raise TimeoutError(f'Connected event not received after {timeout} sec.')
+
+    def shutdown(self):
+        """Cancels communication tasks and shut down connection."""
+        self._launch_task.cancel()
+
+    async def ready(self, timeout=3.0):
         """Coroutine to verify initial synchronization is complete before continuing."""
-        await self._ready_event.wait()
+        async def _await_ready():
+            await self._ready_event.wait()
+        done, _ = await asyncio.wait([asyncio.create_task(_await_ready())], timeout=timeout)
+        if len(done) == 0:
+            self._launch_task.cancel()
+            try:
+                await self._launch_task
+            except CancelledError:
+                pass
+            except Exception as exc:
+                raise TimeoutError(f'Ready event not received after {timeout} sec.') from exc
+            raise TimeoutError(f'Ready event not received after {timeout} sec.')
 
     async def wait(self):
         """Coroutine used to wait for connection completion."""
-        await asyncio.gather(self._listen_task)
+        await self._launch_task
